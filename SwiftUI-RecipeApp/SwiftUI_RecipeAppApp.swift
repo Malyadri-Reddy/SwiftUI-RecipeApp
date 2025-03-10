cc88d5e22e3538e9ce8edb1ed3fa1a65527fcdf6
Malyadri <malyadri.marthula@abbott.com>
2025-03-10 10:36:01 +0530

    Initial Commit

---
+++ SwiftUI-RecipeApp/SwiftUI_RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RecipeAppApp.swift
+//  SwiftUI-RecipeApp
+//
+//  Created by Malyadri Reddy on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-RecipeAppTests/SwiftUI_RecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RecipeAppTests.swift
+//  SwiftUI-RecipeAppTests
+//
+//  Created by Malyadri Reddy on 10/03/25.
+//
+
+import Testing
+@testable import SwiftUI_RecipeApp
+
+struct SwiftUI_RecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
